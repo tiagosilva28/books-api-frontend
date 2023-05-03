45ce48009af5c8a38842f36d01757b75811924b0
tiagosilva28 <tiagosilva28@live.com.pt>
2023-05-03 11:17:23 +0100

    login page

--- src/auth/index.js
+++ src/auth/index.js
@@ -51,3 +51,4 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import LoginPage from "./auth/index"
+
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
